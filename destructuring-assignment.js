b303541e7b8a9ac9f17bd6497851f48222502d05
FeraVerto <themightymasha@gmail.com>
2020-03-05 17:38:40 +0500

    Решение задач по теме Деструктурирующее присваивание

---
+++ destructuring-assignment.js
@@ -0,0 +1,104 @@
+У нас есть объект:
+
+let user = {
+    name: "John",
+    years: 30
+};
+Напишите деструктурирующее присваивание, которое:
+
+свойство name присвоит в переменную name.
+свойство years присвоит в переменную age.
+свойство isAdmin присвоит в переменную isAdmin(false, если нет 
+такого свойства)
+Пример переменных после вашего присваивания:
+
+let user = { name: "John", years: 30 };
+
+// ваш код должен быть с левой стороны:
+// ... = user
+
+let { name, years: age, isAdmin = false } = user;
+
+alert(name); // John
+alert(age); // 30
+alert(isAdmin); // false
+
+
+
+
+
+
+
+
+
+
+Максимальная зарплата
+важность: 5
+У нас есть объект salaries с зарплатами:
+
+let salaries = {
+    "John": 100,
+    "Pete": 300,
+    "Mary": 250
+};
+Создайте функцию topSalary(salaries), которая возвращает имя самого
+высокооплачиваемого сотрудника.
+
+Если объект salaries пустой, то нужно вернуть null.
+Если несколько высокооплачиваемых сотрудников, можно вернуть любого из них.
+    P.S.Используйте Object.entries и деструктурирование, чтобы перебрать пары
+ключ / значение.
+
+let salaries = {
+    "John": 100,
+    "Pete": 300,
+    "Mary": 250
+};
+
+//Решение
+
+function topSalary(salaries) {
+    let arr = Object.entries(salaries);
+
+    let max = 0;
+    let name = null;
+
+    for (let value of arr) {
+        let [name1, number] = value;
+        if (number > max) {
+            max = number;
+            name = name1;
+        }
+    }
+
+    return `${name}: ` + max;
+}
+
+alert(topSalary(salaries));
+
+//Решение
+function topSalary(salaries) {
+    return Math.max.apply(null, Object.values(salaries));
+}
+
+alert(topSalary(salaries));
+
+
+//Решение от learnjavascript.ru
+
+function topSalary(salaries) {
+  
+    let max = 0;
+    let maxName = null;
+  
+    for(const [name, salary] of Object.entries(salaries)) {
+      if (max < salary) {
+        max = salary;
+        maxName = name;
+      }
+    }
+  
+    return maxName;
+  }
+
+  //То же самое, только без объявления переменных
